4ad2d55f6c428dca2ab99af0b4ca1400b0671239
joelfrutos9-byte <joelfrutos9@gmail.com>
2026-07-27 22:18:25 -0300

    Add PELEAS data for upcoming fight

---
+++ src/src/data/elRing.js
@@ -0,0 +1,12 @@
+export const PELEAS = [
+  {
+    id: "fight-01",
+    fecha: "2026-04-04",
+    rival: "Por confirmar",
+    lugar: "Club Evaristo Carriego - Rosario",
+    competencia: "Amateur",
+    categoria: "Peso Gallo (123 LBS)",
+    resultado: "Por confirmar",
+    metodo: "-"
+  }
+];
